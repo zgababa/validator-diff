5824b5877c43d25e9abac0abf480a6f73f16aaff
Fabien de Maestri <fabien.demaestri@gmail.com>
2016-07-08 17:52:29 +0200

    init commit

---
+++ jsonschema.spec.js
@@ -0,0 +1,44 @@
+"use strict";
+
+const Ajv = require("ajv");
+const Promise = require("bluebird");
+const expect = require("chai").expect;
+
+const release = require("./release.json");
+const schema = require("./schema.json");
+
+function getReleaseErrors() {
+  const ajv = new Ajv({allErrors : true, verbose : true});
+  ajv.validate(schema, release);
+  return ajv.errors;
+}
+
+function isAlreadyTreated(currentError) {
+  return (error) => error.dataPath.includes(currentError.dataPath) && currentError.dataPath !== "";
+}
+
+function cleanErrors(errors, currentError) {
+  const reducedErrors = errors.map((error) => isAlreadyTreated(error)(currentError) ? currentError : error);
+  return reducedErrors.some(isAlreadyTreated(currentError)) ? reducedErrors : reducedErrors.concat(currentError);
+}
+
+function validateRelease() {
+  const validationErrors = getReleaseErrors(release, schema);
+  if (!validationErrors) {
+    return Promise.resolve();
+  }
+
+  const error = new Error("validation failed");
+  error.json = JSON.stringify(validationErrors.reduce(cleanErrors, []), null, 2);
+  return Promise.reject(error);
+}
+
+
+describe("JSON SCHEMA", () => {
+
+  it("Should not have errors", () => {
+    return expect(validateRelease).then((t) => {
+      console.log("<<<<<<", t);
+    });
+  });
+});
