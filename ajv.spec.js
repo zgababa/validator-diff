c6636db1badd56d94ce7caefc1157e6b94a369c7
Fabien de Maestri <fabien.demaestri@gmail.com>
2016-08-01 17:32:35 +0200

    json schema validation

---
+++ ajv.spec.js
@@ -0,0 +1,40 @@
+"use strict";
+
+const Ajv = require("ajv");
+const Promise = require("bluebird");
+const expect = require("chai").expect;
+
+const release = require("./release.json");
+const schema = require("./schema.json");
+
+function getReleaseErrors() {
+  const ajv = new Ajv({allErrors : true, verbose : true});
+  const t = ajv.validate(schema, release);
+  return ajv.errors;
+}
+
+function isAlreadyTreated(currentError) {
+  return (error) => error.dataPath.includes(currentError.dataPath) && currentError.dataPath !== "";
+}
+
+function cleanErrors(errors, currentError) {
+  const reducedErrors = errors.map((error) => isAlreadyTreated(error)(currentError) ? currentError : error);
+  return reducedErrors.some(isAlreadyTreated(currentError)) ? reducedErrors : reducedErrors.concat(currentError);
+}
+
+function validateRelease() {
+  const validationErrors = getReleaseErrors(release, schema);
+  if (!validationErrors) {
+    return Promise.resolve();
+  }
+
+  const error = JSON.stringify(validationErrors.reduce(cleanErrors, []), null, 2);
+  return Promise.reject(error);
+}
+
+
+describe("AJV", () => {
+  it("Should not have errors", () => {
+    return expect(validateRelease());
+  });
+});
--- jsonschema.spec.js
+++ jsonschema.spec.js
@@ -1,41 +1,19 @@
 "use strict";
 
-const Ajv = require("ajv");
+const jsonschema = require("jsonschema").Validator;
 const Promise = require("bluebird");
 const expect = require("chai").expect;
 
 const release = require("./release.json");
 const schema = require("./schema.json");
 
-function getReleaseErrors() {
-  const ajv = new Ajv({allErrors : true, verbose : true});
-  const t = ajv.validate(schema, release);
-  return ajv.errors;
-}
-
-function isAlreadyTreated(currentError) {
-  return (error) => error.dataPath.includes(currentError.dataPath) && currentError.dataPath !== "";
-}
-
-function cleanErrors(errors, currentError) {
-  const reducedErrors = errors.map((error) => isAlreadyTreated(error)(currentError) ? currentError : error);
-  return reducedErrors.some(isAlreadyTreated(currentError)) ? reducedErrors : reducedErrors.concat(currentError);
-}
-
-function validateRelease() {
-  const validationErrors = getReleaseErrors(release, schema);
-  if (!validationErrors) {
-    return Promise.resolve();
-  }
-
-  const error = JSON.stringify(validationErrors.reduce(cleanErrors, []), null, 2);
-  return Promise.reject(error);
-}
-
-
-describe("JSON SCHEMA", () => {
-
+describe.only("JSON schema", () => {
   it("Should not have errors", () => {
-    return expect(validateRelease());
+    const v = new jsonschema();
+    const validation = v.validate(release, schema);
+    if (validation.errors.length) {
+      console.log(validation.errors);
+    }
+    expect(validation.errors).to.be.empty;
   });
 });
